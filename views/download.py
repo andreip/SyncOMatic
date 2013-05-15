28cbc11e6190c78253e905c2f9d03e8447d66d87
Andrei Petre <p31andrei@gmail.com>
2013-05-15 19:48:40 +0300

    Indent download file with spaces and delete .hidden file

--- views/download.py
+++ views/download.py
@@ -4,11 +4,10 @@ from flask import render_template, make_response
 from views.render_template import RenderTemplateView
 
 class DownloadView(RenderTemplateView):
-	def __init__(self, *args, **kwargs):
-		super(DownloadView, self).__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super(DownloadView, self).__init__(*args, **kwargs)
 
-	def dispatch_request(self):
-		from run import app
-		files = os.listdir(app.config['UPLOAD_FOLDER'])
-		files.remove(".hidden")
-		return render_template('download.html', files = files)
+    def dispatch_request(self):
+        from run import app
+        files = os.listdir(app.config['UPLOAD_FOLDER'])
+        return render_template('download.html', files = files)
