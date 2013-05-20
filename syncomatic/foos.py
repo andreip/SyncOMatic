13c33133f1cb579859cc8b11d543c21c4514797e
mihaineacsu <mihai.neacsu@ymail.com>
2013-05-20 14:26:07 +0300

    get_filelist() also returns is_dir field

--- syncomatic/foos.py
+++ syncomatic/foos.py
@@ -1,22 +1,25 @@
 import os
 import datetime
+
 from syncomatic import app, lm
 from syncomatic.models import User
 
-def get_filelist(base_path):
-	"""
-		Returns a list of dictionaries;
-		Each dictionary contains the following keys:
-		'name', 'size', 'creation_date'
-	"""
-	filenames = os.listdir(base_path)
-	files = []
-	for name in filenames:
-		# os.stat returns a 10-tuple
-		stats = os.stat(os.path.join(base_path, name))
-		size = stats[6]
-		creation_date = datetime.datetime.fromtimestamp(stats[-2]).strftime("%H:%M %d.%m.%Y")
-		f = {'name': name, 'size': size, 'creation_date': creation_date}
-		files.append(f)
+def get_filelist(path):
+    """
+        Returns a list of dictionaries;
+        Each dictionary contains the following keys:
+        'name', 'size', 'creation_date', 'is_dir'
+    """
+    filenames = os.listdir(path)
+    files = []
+    for name in filenames:
+        # os.stat returns a 10-tuple
+        stats = os.stat(os.path.join(path, name))
+        size = stats[6]
+        creation_date = datetime.datetime.fromtimestamp(stats[-2]).strftime("%H:%M %d.%m.%Y")
+        is_dir = os.path.isdir(path)
+        f = {'name': name, 'size': size, 'creation_date': creation_date,
+            'is_dir': is_dir}
+        files.append(f)
 
-	return files
+    return files
